5922a8938c9aaf0bc913122f6299a32369341ec3
Siddharth Sham <siddhsham@gmail.com>
2017-10-19 17:13:02 +0530

    Update wolfram.py

--- wolfram.py
+++ wolfram.py
@@ -3,5 +3,5 @@ import requests, json
 
 def query(query):
     question = query.replace(" ","+")
-    url = "http://api.wolframalpha.com/v1/simple?appid=TTJ85E-56VP8XY45J&i=" + question + "&format=image"
+    url = "http://api.wolframalpha.com/v1/simple?appid=APP_ID=" + question + "&format=image"
     return url
