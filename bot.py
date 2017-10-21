23dfb2280434a4e6839b71b8c1a2d64a56d619a8
Siddharth Sham <siddhsham@gmail.com>
2017-10-22 00:00:17 +0530

    Update bot.py
    Remove # symbols to add Wikipedia functionality. 
    
    Note: This will break the Wolfram|Alpha functionality. Implement either one at a time. An update to simultaneously use both is on the way.

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 import logging
-import wolfram
+import wolfram, Wiki
 import requests
 from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 
-updater = Updater(config.token)
+updater = Updater(token = congig.TOKEN)
 dispatcher = updater.dispatcher
 
 logging.basicConfig(format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -12,10 +12,16 @@ def start(bot, update):
     bot.send_message(chat_id = update.message.chat_id, text="Hi! What can I do for you?")
 
 def query(bot, update):
-    bot.send_document(chat_id = update.message.chat_id, document = wolfram.query(update.message.text))
+   bot.send_document(chat_id = update.message.chat_id, document = wolfram.query(update.message.text))
+
+#def wikipedia(bot, update):
+#   bot.send_message(chat_id = update.message.chat_id, text = Wiki.wiki(update.message.text))
 
 query_handler = MessageHandler(Filters.text, query)
 dispatcher.add_handler(query_handler)
+
+#wikipedia_handler = MessageHandler(Filters.text, wikipedia)
+#dispatcher.add_handler(wikipedia_handler)
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
