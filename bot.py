74d1d42b94dcbde4fc7ae1875594579b4cb2b710
Siddharth Sham <siddhsham@gmail.com>
2017-10-27 06:25:06 +0530

    Update bot.py
    Updated comments, improved instructions.

--- bot.py
+++ bot.py
@@ -14,14 +14,17 @@ def start(bot, update):
 def query(bot, update):
    bot.send_document(chat_id = update.message.chat_id, document = wolfram.query(update.message.text))
 
+#uncomment the following lines to enable the Wikipedia module. NOTE: It cannot be run alongside the Wolfram module.
+#SUGGESTED: Add a filter to handle queries starting with "wiki" seperately from normal queries.
+
 #def wikipedia(bot, update):
 #   bot.send_message(chat_id = update.message.chat_id, text = Wiki.wiki(update.message.text))
 
-query_handler = MessageHandler(Filters.text, query)
-dispatcher.add_handler(query_handler)
-
 #wikipedia_handler = MessageHandler(Filters.text, wikipedia)
 #dispatcher.add_handler(wikipedia_handler)
+
+query_handler = MessageHandler(Filters.text, query)
+dispatcher.add_handler(query_handler)
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
