f0cf842cbf3b287a88aa820e61a6599f8b6062c0
Siddharth Sham <siddhsham@gmail.com>
2017-10-28 15:18:42 +0530

    Update wolfram.py

--- wolfram.py
+++ wolfram.py
@@ -1,6 +1,11 @@
-import requests, json
+import config
+
+#This module is used for connecting to the Wolfram Alpha API
+#It defines a function that passes the query via an URL
+#returns data as gif/jpeg. Very successful in initial tests.
 
 def query(query):
+    #query = query[7:]                                                      #dormant function. Integration pending.
     question = query.replace(" ","+")
-    url = "http://api.wolframalpha.com/v1/simple?appid=APP_ID=" + question + "&format=image"
-    return url
+    return  "http://api.wolframalpha.com/v1/simple?appid={}&i=".format(config.APP_ID) + question + "&format=image"
+    
