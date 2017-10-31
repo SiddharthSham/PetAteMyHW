6d6f31e7742af1875b7f64a90f4bf58095e75a1f
Siddharth Sham <siddhsham@gmail.com>
2017-11-01 00:09:31 +0530

    Completely overhauled code
    Completed integration of the sub-modules.

--- bot.py
+++ bot.py
@@ -1,45 +1,56 @@
 import logging
-import wolfram, Wiki, config, chatter
+import wolfram, Wiki, config, chatter, OCR
 from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
-#from chatterbot.filters import filter
 
 #This is the main module which oversees the bot.
-#All controls for activating/deactivating bot features are stored here
-#Fragile. Handle with care.
+#API related functions are called seperately. *Only* functions directly affecting the bot are stored here.
+#Fragile. Handle with care. :P
 
-#NOTE: Exception handling is to be added in all modules.
+#TODO: Exception handling is to be added in all modules.
+#TODO: Add OCR module
 
 updater = Updater(token = config.TOKEN)
 dispatcher = updater.dispatcher
 
 logging.basicConfig(format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-def start(bot, update):
-    bot.send_message(chat_id = update.message.chat_id, text="Hi! What can I do for you")
+commands = ["wiki", "wolfram"]              #index of commands
 
-def query(bot, update):
-   bot.send_document(chat_id = update.message.chat_id, document = wolfram.query(update.message.text))
+"""Start of function definitions"""
 
-#uncomment the following lines to enable Wikipedia/Chatting functions.
-#NOTE: Enable only ONE function at a time. Multiple functions will not work simultaneously.
-#SUGGESTED: Write a filter to identify wolfram/wiki queries and handle them separately.
+def checker(bot, update):
+    text = update.message.text              #gets the user input
+    text = text.split(" ")[0]               #stores the first word of the input
+    given = update.message.text
+    if text in commands:                    #this block checks the command used and passes control accordingly
+        if text == "wolfram":               
+            bot.send_document(chat_id = update.message.chat_id, document = wolfram.query(given[8:]))
+        else:
+            bot.send_message(chat_id = update.message.chat_id, text = Wiki.wiki(given[5:]))
+    else: 
+        bot.send_message(chat_id = update.message.chat_id, text = chatter.chat(update.message.text))
 
-#def wikipedia(bot, update):
-#    bot.send_message(chat_id = update.message.chat_id, text = Wiki.wiki(update.message.text))
+def start(bot, update):                      #The user is greeted by this message. A quick start guide, in essence.
+    bot.send_message(chat_id = update.message.chat_id, text="Hi! What can I do for you? Use these commands to talk to me:")
+    bot.send_message(chat_id = update.message.chat_id, text="Start a sentence with \"wolfram\" to to get a mathematical or scientific answer")
+    bot.send_message(chat_id = update.message.chat_id, text="Start a sentence with \"wiki\" to search Wikipedia for some information.")
+    bot.send_message(chat_id = update.message.chat_id, text="...or we could just talk away our time, eh? ;)")
 
-#def chatter(bot, update):
-#    bot.send_message(chat_id = update.message.chat_id, text = chatter.chat(update.message.text))
+#def ocr(bot, update):                                          
+#    bot.send_message(chat_id = update.message.chat_id, text = OCR.ocr(update.message.text))
 
-wolfram_handler = MessageHandler(Filters.text, query)
-dispatcher.add_handler(wolfram_handler)
+"""End of function definitions"""
 
-#wikipedia_handler = MessageHandler(filter.statement_response_list_contains("wiki"), wikipedia)
-#dispatcher.add_handler(wikipedia_handler)
+"""Start of handlers"""
+query_handler = MessageHandler(Filters.text, checker)           #handles textual queries
+dispatcher.add_handler(query_handler)
 
-#chatter_handler = MessageHandler(Filters.text, chatter)
-#dispatcher.add_handler(chatter_handler)
+#ocr_handler = MessageHandler(Filters.photo, ocr)               #(theoretically)handles queries in the form of images 
+#dispatcher.add_handler(ocr_handler)
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
+
+"""End of handlers"""
 
 updater.start_polling()
