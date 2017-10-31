e65b497a3990587f659e9230775298d8d985104e
Siddharth Sham <siddhsham@gmail.com>
2017-11-01 00:10:23 +0530

    Update Wiki.py
    Added comments, code clean-up

--- Wiki.py
+++ Wiki.py
@@ -5,10 +5,10 @@ import json, requests
 #NOTE: The query is successful only if the query exactly matches the title of the Wikipedia page.
 
 def wiki(query):
-    #query = query[4:]                                                     #dormant function. New features coming soon.
-    query = query.replace(" ", "%20")
+    query = query.replace(" ", "%20")                              #performs percentage encoding
     url = "https://en.wikipedia.org/w/api.php?format=json&utf8=&action=query&prop=extracts&exintro=&explaintext=&titles=" + query
-    response = requests.get(url)
-    content = json.loads(response.content.decode("utf-8"))
-    n = str(content['query']['pages'].keys())[12:-3]
-    return content['query']['pages'][n]['extract']
+                                                                   #constructs the URL gor calling the API
+    response = requests.get(url)                                   #posts a request to the constructed URL
+    content = json.loads(response.content.decode("utf-8"))         #stores server response as JSON data
+    n = str(content['query']['pages'].keys())[12:-3]               #Finds the requisite key. Quirk of the Wikimedia API.
+    return  content['query']['pages'][n]['extract']                #Finally, returns the contents we want. Yay!
