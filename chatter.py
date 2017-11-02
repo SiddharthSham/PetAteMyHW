3a44901dd8cb498a7c21eaac99f6b73f574d8f1b
Siddharth Sham <siddhsham@gmail.com>
2017-11-02 23:34:16 +0530

    Update chatter.py

--- chatter.py
+++ chatter.py
@@ -1,10 +1,12 @@
 from chatterbot import ChatBot
 
-#This module is designed to allow the bot to converse in natural language with the user.
+#This module is designed to allow the bot to converse in *somewhat* natural language with the user.
 #It uses the ChatterBot machine learning library to accomplish this
 
 #NOTE: The bot is very naive and susceptible to user input. It quickly learns from the user, sometimes leading to inaccuracy.
 #NOTE: A vast expansion in the number of users should quickly solve this.
+
+#UPDATE: Training has been shifted to the Ubuntu Corpus. Above mentioned problem has been slightly minimised.
 
 chatbot = ChatBot(
     'SidiousBot',
@@ -13,13 +15,11 @@ chatbot = ChatBot(
     #    "chatterbot.logic.TimeLogicAdapter",
     #    "chatterbot.logic.BestMatch"
     #],
-    #input_adapter="chatterbot.input.VariableInputTypeAdapter",            #not yet tested                         
-    #output_adapter="chatterbot.output.OutputAdapter", 
-    trainer='chatterbot.trainers.ChatterBotCorpusTrainer'                  #does not work with ubuntu corpus. Why?
+    trainer='chatterbot.trainers.UbuntuCorpusTrainer'                     #Works! Yay! Bot training is exponentially faster now.
 )
 
-chatbot.train('chatterbot.corpus.english')                                #change this part to use your own training module.                    
-#chatbot.train()                                                          #returns data in a funky way. Usable, but not yet. Needs more analysis.
+#chatbot.train('chatterbot.corpus.english.conversations')                 #Should I write a custom training module here?    #Nvm, no time left now.                    
+chatbot.train()                                                           #Analysis complete. Some modifications allowed proper retrieval of responses. Major upgrade!!
 
 def chat(update):
-    return str(chatbot.get_response(update))
+    return (str(chatbot.get_response(update)))
