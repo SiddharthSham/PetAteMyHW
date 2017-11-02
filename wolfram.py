fe921cb02f933e9c831e9a0cadc4376aec7c8ea2
Siddharth Sham <siddhsham@gmail.com>
2017-11-02 23:28:57 +0530

    Update wolfram.py

--- wolfram.py
+++ wolfram.py
@@ -2,7 +2,7 @@ import config
 
 #This module is used for calling the Wolfram Alpha API
 #It defines a function that constructs an URL based on the query.
-#NOTE: This module returns only the URL. This URL is passed in the bot.py file
+#NOTE: This module returns only the URL. This URL is passed in the bot.py file. Telegram Takes care of the rest.
 
 def query(query):
     question = query.replace(" ","+")                   #plus encoding
